ccf1d9272e43d8beb8fadb7ec5fad683450e8f1a
Ariela Lopes <arieladlopes@gmail.com>
2022-02-22 14:56:55 -0300

    Criação da pasta Model

---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,9 @@
+export class Usuario{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public foto: string
+    public tipo: string
+    public token: string
+}
